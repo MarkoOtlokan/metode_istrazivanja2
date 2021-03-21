b3bdf4cb02430afa26faa88777a7d2810c59e022
MarkoOtlokan <otlokanmarko@gmail.com>
2021-03-21 21:15:48 +0100

    fin

--- metode.py
+++ metode.py
@@ -121,10 +121,225 @@ plt.show()
 ######################################################################################3
 
 
-columns = [delivery_time_opinion, finance_opinion, functionality_opinion, delivery_quality_opinion]
+columns = ['TIME', 'MONEY', 'FUNCTIONALITY', 'QUALITY']
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20, 10))
 for i, ax in enumerate(axes.flatten()):
   col = columns[i]
   ax.pie(col.values, labels=col.index, autopct='%.2f%%')
   ax.set_title(col.name)
 plt.show();
+
+
+
+print('-------priority_stack_time-----------')
+priority_stack_time = survey['priority_stack_time'].value_counts()
+print(priority_stack_time.sort_values(ascending=False))
+print('\n-------priority_stack_money-----------------')
+priority_stack_money = survey['priority_stack_money'].value_counts()
+print(priority_stack_money.sort_values(ascending=False))
+print('\n------priority_stack_functionality-----------')
+priority_stack_functionality = survey['priority_stack_functionality'].value_counts()
+print(priority_stack_functionality.sort_values(ascending=False))
+print('\n-------priority_stack_quality--------')
+priority_stack_quality = survey['priority_stack_quality'].value_counts()
+print(priority_stack_quality.sort_values(ascending=False))
+
+
+columns = ['priority_stack_time', 'priority_stack_money', 'priority_stack_functionality', 'priority_stack_quality']
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(30, 10))
+for i, col in enumerate(columns):
+  sns.countplot(x=survey[col],ax=axes[i//2, i%2])
+plt.show()
+
+
+print("WM - time: " + str(((priority_stack_time[2] * 4) + (priority_stack_time[1] * 3) + (priority_stack_time[0] * 2) + priority_stack_time[3])/160))
+print("WM - money: " + str(((priority_stack_money[3] * 4) + (priority_stack_money[2] * 3) + (priority_stack_money[1] * 2) + priority_stack_money[0])/160))
+print("WM - functionality: " + str(((priority_stack_functionality[0] * 4) + (priority_stack_functionality[1] * 3) + (priority_stack_functionality[2] * 2) + priority_stack_functionality[3])/160))
+print("WM - quality: " + str(((priority_stack_quality[2] * 4) + (priority_stack_quality[1] * 3) + (priority_stack_quality[3] * 2) + priority_stack_quality[0])/160))
+
+
+
+survey.columns
+
+
+
+adhoc_successful = survey.adhoc_successful.value_counts()
+adhoc_challenged = survey.adhoc_challenged.value_counts()
+adhoc_failed = survey.adhoc_failed.value_counts()
+
+print("AdHoc - succesful")
+print(adhoc_successful)
+print("\n AdHoc - challenged")
+print(adhoc_challenged)
+print("\n AdHoc - failed")
+print(adhoc_failed)
+
+data = [adhoc_successful, adhoc_challenged, adhoc_failed]
+fig = plt.figure(figsize=(17,17))
+rows = 2
+cols = 2
+for i, feature in enumerate(data):
+  fig.add_subplot(rows, cols, i+1)
+  plt.pie(feature.values, labels=feature.index, autopct='%.2f%%')
+  plt.title(feature.name)
+plt.show()
+
+iterative_successful = survey.iterative_successful.value_counts()
+iterative_challenged = survey.iterative_challenged.value_counts()
+iterative_failed = survey.iterative_failed.value_counts()
+
+print("Iterative - succesful")
+print(iterative_successful)
+print("\n Iterative - challenged")
+print(iterative_challenged)
+print("\n Iterative - failed")
+print(iterative_failed)
+
+
+
+
+data = [iterative_successful, iterative_challenged, iterative_failed]
+fig = plt.figure(figsize=(17,17))
+rows = 2
+cols = 2
+for i, feature in enumerate(data):
+  fig.add_subplot(rows, cols, i+1)
+  plt.pie(feature.values, labels=feature.index, autopct='%.2f%%')
+  plt.title(feature.name)
+plt.show()
+
+
+
+
+
+agile_successful = survey.agile_successful.value_counts()
+agile_challenged = survey.agile_challenged.value_counts()
+agile_failed = survey.agile_failed.value_counts()
+
+print("Agile - succesful")
+print(agile_successful)
+print("\n Agile - challenged")
+print(agile_challenged)
+print("\n Agile - failed")
+print(agile_failed)
+
+
+
+
+
+data = [agile_successful, agile_challenged, agile_failed]
+fig = plt.figure(figsize=(17,17))
+rows = 2
+cols = 2
+for i, feature in enumerate(data):
+  fig.add_subplot(rows, cols, i+1)
+  plt.pie(feature.values, labels=feature.index, autopct='%.2f%%')
+  plt.title(feature.name)
+plt.show()
+
+
+
+
+
+traditional_successful = survey.traditional_succesful.value_counts()
+traditional_challenged = survey.traditional_challenged.value_counts()
+traditional_failed = survey.traditional_failed.value_counts()
+
+print("Traditional - succesful")
+print(traditional_successful)
+print("\n Traditional - challenged")
+print(traditional_challenged)
+print("\n Traditional - failed")
+print(traditional_failed)
+
+
+
+
+
+data = [traditional_successful, traditional_challenged, traditional_failed]
+fig = plt.figure(figsize=(17,17))
+rows = 2
+cols = 2
+for i, feature in enumerate(data):
+  fig.add_subplot(rows, cols, i+1)
+  plt.pie(feature.values, labels=feature.index, autopct='%.2f%%')
+  plt.title(feature.name)
+plt.show()
+
+
+
+
+columns =  ['adhoc_successful', 'adhoc_challenged', 'adhoc_failed',
+       'iterative_successful', 'iterative_challenged', 'iterative_failed',
+       'agile_successful', 'agile_challenged', 'agile_failed',
+       'traditional_succesful', 'traditional_challenged',
+       'traditional_failed']
+
+data = []
+for col in columns:
+  helper = survey[survey[col] != "Don't Know"]
+  data.append({'count': helper[col].count(), 'valueCount' : helper[col].value_counts(sort=True)})
+
+
+# Moglo je elegantnije, ali nisam imao vremena...
+def calcWeightSum(x):
+  switcher = {
+    0: 1,
+    '1-10%': 2,
+    '11-20%': 3,
+    '21-30%': 4,
+    '31-40%': 5,
+    '41-50%': 6,
+    '51-60%': 7,
+    '61-70%': 8,
+    '71-80%': 9,
+    '81-90%': 10,
+    '91-100%': 11,
+  }
+
+  length = len(x.index)
+
+  i = 0
+  sum = 0
+  while i < length:
+    sum += x.values[i] * switcher.get(x.index[1], 1)
+    i += 1
+
+  return sum
+
+wAvgMean = {}
+i = 0
+while i < len(data):
+  wAvgMean.update({data[i]['valueCount'].name: (calcWeightSum(data[i]['valueCount'])/data[i]['count'])})
+  i += 1
+
+
+adhoc = {}
+agile = {}
+traditional = {}
+iterative = {}
+for k, v in wAvgMean.items():
+  if 'adhoc' in k:
+    adhoc.update({k:v})
+  elif 'agile' in k:
+    agile.update({k:v})
+  elif 'traditional' in k:
+    traditional.update({k:v})
+  elif 'iterative' in k:
+    iterative.update({k:v})
+
+agileDF = pd.DataFrame.from_dict(list(agile.items()))
+adhocDF = pd.DataFrame.from_dict(list(adhoc.items()))
+traditionalDF = pd.DataFrame.from_dict(list(traditional.items()))
+iterativeDF = pd.DataFrame.from_dict(list(iterative.items()))
+
+
+
+data = [agileDF, adhocDF, traditionalDF, iterativeDF]
+fig = plt.figure(figsize=(17,17))
+rows = 2
+cols = 2
+for i, feature in enumerate(data):
+  fig.add_subplot(rows, cols, i+1)
+  plt.pie(feature[1], labels=feature[0], autopct='%.2f%%')
+plt.show()
